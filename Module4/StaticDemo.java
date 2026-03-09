6a504b73a300441a90091724825285b6c19adff0
kavanaaradhya16-lang <kavanaaradhya16@gmail.com>
2026-03-09 08:40:11 +0530

    Add files via upload

---
+++ Module4/StaticDemo.java
@@ -0,0 +1,14 @@
+package Module4;
+
+public class StaticDemo {
+    static int count = 0;
+
+    static void display() {
+        System.out.println("Count = " + count);
+    }
+
+    public static void main(String[] args) {
+        count++;
+        display();
+    }
+}
