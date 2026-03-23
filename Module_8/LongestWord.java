160720fb768162e26e01da607ca42991ad7d9132
kavanaaradhya16-lang <kavanaaradhya16@gmail.com>
2026-03-23 12:00:27 +0530

    Add files via upload

---
+++ Module_8/LongestWord.java
@@ -0,0 +1,18 @@
+package Module_8;
+    class LongestWord {
+        public static void main(String[] args) {
+            String str = "Java programming is interesting";
+            String[] words = str.split(" ");
+
+            String longest = "";
+
+            for (String word : words) {
+                if (word.length() > longest.length()) {
+                    longest = word;
+                }
+            }
+
+            System.out.println("Longest word: " + longest);
+        }
+    }
+
---
+++ Module_8/ReplaceSpaces.java
@@ -0,0 +1,10 @@
+package Module_8;
+    class ReplaceSpaces {
+        public static void main(String[] args) {
+            String str = "Hello World Java";
+            str = str.replace(" ", "%20");
+
+            System.out.println(str);
+        }
+    }
+
